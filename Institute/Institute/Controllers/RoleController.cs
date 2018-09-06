c7a6f04266cb9b73f811c3f2cb6404ebe2c51e47
kapil-valecha <kapil.valecha@systematixindia.com>
2018-09-06 19:47:44 +0530

    Initial commit

---
+++ Institute/Institute/Controllers/RoleController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace Institute.Controllers
+{
+    public class RoleController : Controller
+    {
+        private ApplicationRoleManager _roleManager;
+
+        public RoleController()
+        {
+        }
+
+        public RoleController(ApplicationRoleManager roleManager, ApplicationSignInManager signInManager)
+        {
+            RoleManager = roleManager;
+        }
+
+        public ApplicationRoleManager RoleManager
+        {
+            get
+            {
+                return _roleManager ?? HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
+            }
+            private set
+            {
+                _roleManager = value;
+            }
+        }
+
+
+        // GET: Role
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
